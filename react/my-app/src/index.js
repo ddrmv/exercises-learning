59abf9c45d119e466ceea6bc4daa0df79878e3de
D. Dramchev <ddramchev@gmail.com>
2022-11-16 23:28:35 +0000

    Add example element code, comment old code, save with Prettier

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+/*// original index content
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
+
+/*// render simple element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const element = <h1>Hello World.</h1>;
+root.render(element);
+*/
+
+/*// render time every second (element concept)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const tick = () => {
+  const element = (
+    <div>
+      <h1>Hello world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+};
+
+setInterval(tick, 1000);
+*/
+
+const Welcome = (props) => {
+  return <h1>Hello, {props.name}</h1>;
+};
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {props.name}</h1>;
+//   }
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
