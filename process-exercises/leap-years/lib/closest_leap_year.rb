98b76d91298f3c05f35b6013275d52d0cfa265e0
D. Dramchev <ddramchev@gmail.com>
2022-11-22 19:18:24 +0000

    Return closest year for all cases, validate input

--- process-exercises/leap-years/lib/closest_leap_year.rb
+++ process-exercises/leap-years/lib/closest_leap_year.rb
@@ -3,5 +3,14 @@
 require_relative 'leap_years'
 
 def closest_leap_year(given_year)
-  return given_year if leap_year?(given_year)
+  valid_years?(given_year)
+  
+  difference = 0
+
+  loop do
+    return given_year + difference if leap_year?(given_year + difference)
+    return given_year - difference if leap_year?(given_year - difference)
+
+    difference += 1
+  end
 end
--- process-exercises/leap-years/spec/closest_leap_year_spec.rb
+++ process-exercises/leap-years/spec/closest_leap_year_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe '#closest_leap_year' do
   it 'returns same year if input is leap year' do
     expect(closest_leap_year(2000)).to eq 2000
   end
+
+  it 'returns closest (one down, up)' do
+    expect(closest_leap_year(2001)).to eq 2000
+    expect(closest_leap_year(1999)).to eq 2000
+  end
+
+  it 'returns two up when equidistant from two' do
+    expect(closest_leap_year(2006)).to eq 2008
+  end
+
+  it 'returns correctly around non-leap century' do
+    expect(closest_leap_year(1900)).to eq 1904
+    expect(closest_leap_year(1899)).to eq 1896
+  end
+
+  it 'returns 0 for year 0, as per ISO specification' do
+    expect(closest_leap_year(0)).to eq 0
+  end
+
+  it 'raises error for invalid input' do
+      expect { closest_leap_year(2000.1) }.to raise_error 'Year should be an integer'
+  end
 end
