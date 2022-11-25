62cc516e44de2a01c462d5447dc96bbdc578d5b6
D. Dramchev <ddramchev@gmail.com>
2022-11-25 00:56:20 +0000

    Refactor: remove unnecessary return/assignment

--- process-exercises/ten-minute-walk/lib/ten_minute_walk.rb
+++ process-exercises/ten-minute-walk/lib/ten_minute_walk.rb
@@ -5,8 +5,9 @@ def ten_minute_walk?(walk)
 
   position = { x: 0, y: 0 }
   walk.each do |direction|
-    position = update_position(position, direction)
+    update_position(position, direction)
   end
+
   position[:x].zero? && position[:y].zero?
 end
 
@@ -15,5 +16,4 @@ def update_position(position, direction)
   position[:y] -= 1 if direction == 's'
   position[:x] -= 1 if direction == 'w'
   position[:x] += 1 if direction == 'e'
-  position
 end
