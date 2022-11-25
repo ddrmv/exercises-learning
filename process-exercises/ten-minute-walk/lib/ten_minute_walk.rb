a8375b2fcfbc3f098d2b8cdf4feacf5c21dfe73b
D. Dramchev <ddramchev@gmail.com>
2022-11-25 00:17:26 +0000

    Refactor: Extract updating position to a helper function

--- process-exercises/ten-minute-walk/lib/ten_minute_walk.rb
+++ process-exercises/ten-minute-walk/lib/ten_minute_walk.rb
@@ -2,16 +2,21 @@ def ten_minute_walk?(walk)
   return false if walk.length != 10
   position = {x: 0, y: 0}
   walk.each do |direction|
-    case direction
-    when 'n'
-      position[:y] += 1
-    when 's'
-      position[:y] -= 1
-    when 'w'
-      position[:x] -= 1
-    when 'e'
-      position[:x] += 1
-    end
+    position = update_position(position, direction)
   end
   return position[:x] == 0 && position[:y] == 0
+end
+
+def update_position(position, direction)
+  case direction
+  when 'n'
+    position[:y] += 1
+  when 's'
+    position[:y] -= 1
+  when 'w'
+    position[:x] -= 1
+  when 'e'
+    position[:x] += 1
+  end
+  position
 end
