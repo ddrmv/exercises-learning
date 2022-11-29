75d1667797bb7e069082de0473700fe6863765b8
D. Dramchev <ddramchev@gmail.com>
2022-11-29 23:38:09 +0000

    Add correct value for each letter

--- process-exercises/scrabble-solver/scrabble.js
+++ process-exercises/scrabble-solver/scrabble.js
@@ -9,7 +9,25 @@ class Scrabble {
       return 0;
     }
     if ("aeioulnrst".includes(this.word)) {
-      this.score = 1;
+      this.score += 1;
+    }
+    if ("dg".includes(this.word)) {
+      this.score += 2;
+    }
+    if ("bcmp".includes(this.word)) {
+      this.score += 3;
+    }
+    if ("fhvwy".includes(this.word)) {
+      this.score += 4;
+    }
+    if ("k".includes(this.word)) {
+      this.score += 5;
+    }
+    if ("jx".includes(this.word)) {
+      this.score += 8;
+    }
+    if ("qz".includes(this.word)) {
+      this.score += 10;
     }
     return this.score;
   };
--- process-exercises/scrabble-solver/scrabble.spec.js
+++ process-exercises/scrabble-solver/scrabble.spec.js
@@ -16,4 +16,19 @@ describe("Scrabble", () => {
     expect(scrabble1.getScore()).toEqual(1);
     expect(scrabble2.getScore()).toEqual(1);
   });
+
+  it("returns correct value for one-letter d/b/f/k/j/q", () => {
+    const scrabble1 = new Scrabble("d");
+    const scrabble2 = new Scrabble("b");
+    const scrabble3 = new Scrabble("f");
+    const scrabble4 = new Scrabble("k");
+    const scrabble5 = new Scrabble("j");
+    const scrabble6 = new Scrabble("q");
+    expect(scrabble1.getScore()).toEqual(2);
+    expect(scrabble2.getScore()).toEqual(3);
+    expect(scrabble3.getScore()).toEqual(4);
+    expect(scrabble4.getScore()).toEqual(5);
+    expect(scrabble5.getScore()).toEqual(8);
+    expect(scrabble6.getScore()).toEqual(10);
+  });
 });
