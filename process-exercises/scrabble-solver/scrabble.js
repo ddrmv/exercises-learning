f9101609387092cf6ed9515bbe8d374d90e9925c
D. Dramchev <ddramchev@gmail.com>
2022-11-29 23:29:12 +0000

    Add correct score for 'a'

--- process-exercises/scrabble-solver/scrabble.js
+++ process-exercises/scrabble-solver/scrabble.js
@@ -1,10 +1,14 @@
 class Scrabble {
   constructor(word) {
     this.word = word;
+    this.score = 0;
   }
 
   getScore = () => {
-    return 0;
+    if (this.word === "a") {
+      this.score = 1;
+    }
+    return this.score;
   };
 }
 
--- process-exercises/scrabble-solver/scrabble.spec.js
+++ process-exercises/scrabble-solver/scrabble.spec.js
@@ -1,8 +1,17 @@
 const Scrabble = require("./scrabble");
 
 describe("Scrabble", () => {
-  it("returns 0 on empty string input", () => {
-    const scrabble = new Scrabble("");
-    expect(scrabble.getScore()).toEqual(0);
+  it("returns 0 for empty string, space, tab, newline", () => {
+    const scrabble1 = new Scrabble("");
+    const scrabble2 = new Scrabble(" \t\n");
+    const scrabble3 = new Scrabble(null);
+    expect(scrabble1.getScore()).toEqual(0);
+    expect(scrabble2.getScore()).toEqual(0);
+    expect(scrabble3.getScore()).toEqual(0);
+  });
+
+  it("returns 1 for 'a'", () => {
+    const scrabble = new Scrabble("a");
+    expect(scrabble.getScore()).toEqual(1);
   });
 });
