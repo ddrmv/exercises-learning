1d71a802b70f31216530d8547a40b1e87042d2c2
D. Dramchev <ddramchev@gmail.com>
2022-12-03 00:35:11 +0000

    Pass tests to populate Shop, return a price

--- process-exercises/checkout/src/shop.test.ts
+++ process-exercises/checkout/src/shop.test.ts
@@ -1,8 +1,16 @@
+import { Item } from "./item";
 import { Shop } from "./shop";
 describe("Shop", () => {
   it("adds item and returns price", () => {
-    // const shop = new Shop();
-    // shop.addItem("A", 50);
-    // expect(shop.checkout("A")).toBe(50);
+    const shop = new Shop();
+    const itemA = new Item("A", 50);
+    const itemB = new Item("B", 30);
+    const itemC = new Item("C", 20);
+    const itemD = new Item("D", 15);
+    shop.addItem(itemA);
+    shop.addItem(itemB);
+    shop.addItem(itemC);
+    shop.addItem(itemD);
+    expect(shop.checkout("A")).toBe(50);
   });
 });
--- process-exercises/checkout/src/shop.ts
+++ process-exercises/checkout/src/shop.ts
@@ -1,1 +1,15 @@
-export class Shop {}
+import { Item } from "./item";
+export class Shop {
+  _items: Item[];
+  constructor() {
+    this._items = [];
+  }
+
+  addItem(item: Item) {
+    this._items.push(item);
+  }
+
+  checkout(shoppingList: string) {
+    return this._items[0].price;
+  }
+}
