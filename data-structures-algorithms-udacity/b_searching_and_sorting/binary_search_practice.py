c55cb71e4e4f6dee9f18619d8112c96cf0240549
D. Dramchev <ddramchev@gmail.com>
2023-01-20 16:55:23 +0000

    Refactor binary search

--- data-structures-algorithms-udacity/b_searching_and_sorting/binary_search_practice.py
+++ data-structures-algorithms-udacity/b_searching_and_sorting/binary_search_practice.py
@@ -10,21 +10,23 @@ elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
+def find_new_half(start, end):
+    return start + ((end - start) // 2)
 
 # this code fails on udacity due to type hints, passes without them
 def binary_search(input_array: list, value: int):
     start: int = 0
     end: int = len(input_array) - 1
-    half: int = start + ((end - start) // 2)
+    half: int = find_new_half(start, end)
     while True:
         if input_array[half] == value:
             return half
         else:
             if value < input_array[half]:
                 end = half
-                half = start + ((end - start) // 2)
+                half = find_new_half(start, end)
             else:
                 start = half
-                half = start + ((end - start) // 2)
+                half = find_new_half(start, end)
         if half == start or half == end:
             return -1
